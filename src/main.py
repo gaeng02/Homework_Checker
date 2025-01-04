b3916529e8ad9200e8368bb211c217c7b7b870ff
gaeng02 <sondin144@gmail.com>
2025-01-04 15:56:13 +0900

    main

---
+++ src/main.py
@@ -0,0 +1,7 @@
+"""
+All processes are carried out in this file :
+    1. Crawling is performed.
+    2. The data is transformed to fit the required format and saved.
+    3. The data is compared with existing data.
+    4. Additional results are recorded.
+"""
