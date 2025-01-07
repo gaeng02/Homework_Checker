9734443befbe329392e292f29f325865d841beb1
gaeng02 <sondin144@gmail.com>
2025-01-07 22:42:45 +0900

    CRUD

--- src/database.py
+++ src/database.py
@@ -3,21 +3,32 @@ import os
 # Create / Read / Update / Delete
 
 # file
-def Create_file () :
-    
+class User :
 
-    
-def save (user_id, data) :
+    def __init__ (self, name, extension = "csv", base = "../data/") :
+        self.name = name
+        self.file = base + name + "." + extension
+        
+    def Create (self) :
+        
+        with open (self.file, mode = "w", newline = "", encoding = "utf-8") as f : 
 
-    file = "../info.csv"
-    
-    with open(file, mode = "r", encoding = "utf-8") as f :
-        reader = csv.reader(f)
-        rows = list(reader)
+            print("Create")
 
-    rows.append(data)
+    def Read (self) :
 
-    with open(file, mode = "w", encoding = "utf-8", newline = "") as f :
+        with open ( ) :
+        
 
-        writer = csv.writer(f)
-        writer.writerows(rows)
+    def Delete (self) :
+
+        try :
+            os.remove(self.file)
+            print("Delete")
+            
+        except FileNotFoundError :
+            print(f"{file} 파일이 존재하지 않습니다.")
+            
+        except Exception as e : 
+            print(f"파일 삭제 중 오류 발생: {e}")
+
